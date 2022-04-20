5fa1b0fb60545bcff78092c293ca0a47f395c203
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-04-20 16:03:34 +0100

    init

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const tasks = require('./final/routes/tasks')
+
+//middleware
+
+app.use(express.json());
 
 //routes
 app.get('/hello', (req, res) => {
     res.send('<h1>Task Manager App</h1>')
 })
+
+app.use('/api/v1/tasks', tasks)
 
 
 /*app.get('/api/v1/tasks')   //-get all the tasks
---
+++ task-manager/final/db/connect.js
@@ -0,0 +1,2 @@
+const connectionString =
+ 'mongodb+srv://katherine:<password>@nodeexpresstutorialclus.trgqf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
