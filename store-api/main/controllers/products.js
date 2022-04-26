a4b28a3182c952f216774bc097b6dc7812217bc2
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-04-26 11:30:53 +0100

    init

--- store-api/main/controllers/products.js
+++ store-api/main/controllers/products.js
@@ -1,10 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products =await Product.find({ 
-        name: ''
+
+    const search = 'ab'
+    const products = await Product.find({
+        name: { $regex: search, $options: 'i' }
     })
-    res.status(200).json({products, nbHits: products.length})
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
@@ -15,18 +17,18 @@ const getAllProducts = async (req, res) => {
         queryObject.featured = featured === 'true' ? true : false
     }
 
-    if(company){
+    if (company) {
         queryObject.company = company
     }
 
-    if(name) {
-        queryObject.name = name
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
     }
     const products = await Product.find(queryObject)
     res.status(200).json({ products, nbHits: products.length })
 }
 
-module.exports ={
+module.exports = {
     getAllProducts,
     getAllProductsStatic
 }
