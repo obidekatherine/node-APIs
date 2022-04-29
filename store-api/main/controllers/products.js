19a40af5a55dcf9068d7fc6fd0136043d59d367b
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-04-29 13:40:38 +0100

    init

--- store-api/main/controllers/products.js
+++ store-api/main/controllers/products.js
@@ -2,7 +2,11 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
 
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(4)
+    .skip(1)//skip the first product on the list
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -37,6 +41,15 @@ const getAllProducts = async (req, res) => {
         const fieldList = fields.split(',').join('')
         result = result.select(fieldList)  
     }
+    //pagination -- how the page is going to divide(next, prev etc)
+    //any page searched for would be listed first
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit;
+
+    result = result.skip(skip).limit(limit)
+
+
     const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }
