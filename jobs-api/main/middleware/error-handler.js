b91a454b899d5633f4e8b057379f8641a3126e63
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-05-08 10:59:43 +0100

    update

--- jobs-api/main/middleware/error-handler.js
+++ jobs-api/main/middleware/error-handler.js
@@ -1,10 +1,32 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
+
+//making mongoose error readable
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    //set default
+    statusCode: err.StatusCodes || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg:err.message || 'Something went wrong, try again'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+
+
+if(err.name === 'ValidationError'){
+  customError.msg = Object.values(err.errors).map((item)=>item.message).join(' and ')
+  customError.statusCode = 400
+}
+
+if(err.name === 'CastError'){
+  customError.msg = `No item found with id : ${err.value}`
+  customError.statusCode = 404
+}
+
+
+if(err.code && err.code === 11000){
+  customError.msg = `${Object.keys(err.keyValue)} already exist`
+  customError.statusCode = 400
+}
+
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
